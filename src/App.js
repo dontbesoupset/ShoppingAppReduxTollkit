5de62ae50840a583a0ecdb24c37d5a737f144ecf
Diana Tishkevich <dontbesoupset@gmail.com>
2023-09-14 13:16:44 +0300

    Firebase connected, now database is changing everytime we add smth to cart or remove

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import { useSelector } from 'react-redux'
 
 function App() {
+  const cart = useSelector(state => state.cart);
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch('https://shoppingcartreduxhttp-default-rtdb.firebaseio.com/cartItems.json', {
+        method: "PUT",
+        body: JSON.stringify(cart)
+      })
+      const data = await res.json();
+    }
+    sendRequest();
+  }, [cart])
+
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {isLoggedIn ? 
+        <Layout /> 
+        : 
+        <Auth />}
+        
     </div>
   );
 }
